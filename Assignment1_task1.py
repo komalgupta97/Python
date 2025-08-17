22a88c01f4b6ffd674649ffa2cf613faaf3766f1
komalgupta97 <157458612+komalgupta97@users.noreply.github.com>
2025-08-17 18:14:21 +0530

    Add files via upload

---
+++ Assignment1_task1.py
@@ -0,0 +1,8 @@
+# Perform Basic Mathematical Operations:
+number1 = int(input('Enter the first number: '))
+number2 = int(input('Enter the second number: '))
+
+print('Addition: ', number1 + number2)
+print('Subtraction: ', number1-number2)
+print('Multiplication: ', number1*number2)
+print('Division: ', number1/number2)
---
+++ Assignment1_task2.py
@@ -0,0 +1,5 @@
+# Create a Personalized Greeting:
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name: ')
+fullname = firstname + ' ' + lastname + '!'
+print('Hello,', fullname, 'Welcome to the Python program.')
